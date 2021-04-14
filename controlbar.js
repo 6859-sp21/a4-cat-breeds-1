ac0702e63083c514a004468a7c7065b1ca46696c
Yunyi Zhu <yunyizhu@mit.edu>
2021-04-14 01:08:11 -0400

    test github io

--- controlbar.js
+++
@@ -1,8 +0,0 @@
-
-  $(window).scroll(function(){ // scroll event
-    var windowTop = $(window).scrollTop(); // returns number
- var $sticky = $('.sticky');
-        $sticky.css({
-        position: 'fixed', top:0
-      });
-})
