0c06b9ceb650381a3b9de6afac17130fa3d92a1b
Yunyi Zhu <yunyizhu@mit.edu>
2021-04-15 10:55:49 -0400

    cat popup page info

--- src/components/catPreview.js
+++ src/components/catPreview.js
@@ -3,9 +3,7 @@ import React from "react";
 const CatPreview = ({cat, selectCat}) => (
   <div className="cat-card" onClick={() => selectCat(cat.breed)}>
     <img src= {process.env.PUBLIC_URL + "/cat_pictures/" +cat.breed + '.jpg'} alt={cat.breed} />
-    {
-      cat.breed
-    }
+    {cat.breed.replace( /([A-Z])/g, " $1" )}
   </div>
 )
 
